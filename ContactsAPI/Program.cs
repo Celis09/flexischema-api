d8f66858dd8cef13bc34c8d3cda0f327eb8b4e48
Celis09 <johncarlocelis09@gmail.com>
2026-06-01 11:42:06 +0800

    chore: update Swagger UI title to FlexiSchema API

--- ContactsAPI/Program.cs
+++ ContactsAPI/Program.cs
@@ -123,7 +123,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new() { Title = "ContactsAPI", Version = "v1" });
+    c.SwaggerDoc("v1", new() { Title = "FlexiSchema API", Version = "v1" });
 
     c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
     {
